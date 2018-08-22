3a0bda9645c33424f5889af5ae102559e0996f49
John Paul Schmidt <mc.wren@gmail.com>
2018-08-22 01:20:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,21 +14,5 @@ function logWhisper(string) {
   console.log('hello')
 }
 
-function sayHiToGrandma(string) {
-  if (string === "HELLO") {
-  return }
-}
+var uppercase = "HELLO!"
 
-function sayHiToGrandma(lowercase) {
-  console.log("I can't hear you!")
-}
-
-
-var uppercase = "YES INDEED!"
-var uppiedoodie = "HELLO"
-
-var lowercase = "hello"
-lowercase.toLowerCase() === lowercase
-
-var mixedcase = "I love you, Grandma."
-mixedcase.toLowerCase() === mixedcase
