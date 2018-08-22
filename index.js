641c21de0791d906e76868549262cb65313ef80b
John Paul Schmidt <mc.wren@gmail.com>
2018-08-22 00:58:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,19 @@ function sayHiToGrandma(uppercase) {
   return "YES INDEED!"
 }
 
+function sayHiToGrandma(lowercase) {
+  return "I can/'t hear you!"
+}
+
+function sayHiToGrandma(mixedcase) {
+  return "I love you, too."
+}
 
 var uppercase = "HELLO!"
 uppercase.toUpperCase() === uppercase
+
+var lowercase = "hello"
+lowercase.toLowerCase() === lowercase
+
+var mixedcase = "I love you, Grandma."
+mixedcase.toLowerCase() === mixedcase
