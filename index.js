8d16140a58635b36ffba37784c3cfeffdb968fba
John Paul Schmidt <mc.wren@gmail.com>
2018-08-22 01:37:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,12 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
+  if (string.toLowerCase() === string)
   return "I can\'t hear you!"
-}
-
-function sayHiToGrandma(uppercase) {
-  return "YES INDEED!"
-}
+  
+  if (string.toUpperCase() === string)
+    return "YES INDEED!"
+    
+  else
+    return "I love you, too." 
+  }
